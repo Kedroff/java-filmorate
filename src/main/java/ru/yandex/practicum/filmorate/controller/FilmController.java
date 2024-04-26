70c04ee2b9bd06784573ef4a2fcbbaf57cbaf4aa
Magicoff <danilka.3012@yandex.ru>
2024-04-26 19:36:33 +0300

    commit

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
@@ -10,7 +8,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
@@ -36,52 +33,7 @@ public class FilmController {
         return films.values();
     }
 
-    @PutMapping
-    public Film update(@RequestBody Film newFilm) {
-        try {
-            if (newFilm.getId() == null) {
-                log.error("Id должен быть указан");
-                throw new ValidationException("Id должен быть указан");
-            }
 
-            try {
-                if (films.containsKey(newFilm.getId())) {
-
-                    validateFilm(newFilm);
-                    Film oldFilm = films.get(newFilm.getId());
-
-                    oldFilm.setName(newFilm.getName());
-                    oldFilm.setDescription(newFilm.getDescription());
-                    oldFilm.setReleaseDate(newFilm.getReleaseDate());
-                    oldFilm.setDuration(newFilm.getDuration());
-                    log.info("Фильм успешно изменен");
-                    return oldFilm;
-                } else {
-                    log.error("Фильма с таким id не существует");
-                }
-            } catch (ValidationException e) {
-                log.error("Ошибка: {}", e.getMessage());
-            }
-        } catch (ValidationException e) {
-            log.error("Ошибка обновления фильма: {}" + e.getMessage());
-        }
-        return null;
-    }
-
-    public static void validateFilm(Film film) throws ValidationException {
-        if (film.getName().isBlank()) {
-            throw new ValidationException("Название не может быть пустым");
-        }
-        if (film.getDescription().length() > 200) {
-            throw new ValidationException("Максимальная длина описания — 200 символов");
-        }
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
-        }
-        if (film.getDuration() <= 0) {
-            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
-        }
-    }
 
     private long getNextId() {
         long currentMaxId = films.keySet()
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -14,6 +13,5 @@ public class Film {
     private String name;
     private String description;
     private LocalDate releaseDate;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Integer duration;
 }
