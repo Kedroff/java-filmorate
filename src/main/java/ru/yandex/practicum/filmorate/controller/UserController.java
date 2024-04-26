04c2f8cf4b86088231013a4b1b418f1e2aa7d753
Magicoff <danilka.3012@yandex.ru>
2024-04-26 19:13:03 +0300

    first commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,95 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        try {
+            validateUser(user);
+            user.setId(getNextId());
+            users.put(user.getId(), user);
+            log.info("Создан новый пользователь: {}", user);
+        } catch (ValidationException e) {
+            log.error("Ошибка создания пользователя: {}", e.getMessage());
+        }
+        return user;
+    }
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+
+        try {
+            if (newUser.getId() == null) {
+                log.error("Id должен быть указан");
+                throw new ValidationException("Id должен быть указан");
+            }
+            try {
+                if (users.containsKey(newUser.getId())) {
+
+                    validateUser(newUser);
+                    User oldUser = users.get(newUser.getId());
+
+                    oldUser.setEmail(newUser.getEmail());
+                    oldUser.setLogin(newUser.getLogin());
+                    oldUser.setBirthday(newUser.getBirthday());
+                    log.info("Пользователь успешно изменен");
+                    return oldUser;
+                } else {
+                    log.error("Пользователя с таким id не существует");
+                }
+            } catch (ValidationException e) {
+                log.error("Ошибка обновления пользователя: {}", e.getMessage());
+            }
+
+        } catch (ValidationException e) {
+            log.error("Ошибка обновления пользователя {}" + e.getMessage());
+        }
+        return null;
+    }
+
+    public static void validateUser(User user) throws ValidationException {
+        if (user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Email не должен быть пустым и должен содержать @");
+        }
+        if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Login не должен быть пустым и содержать пробелы");
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем.");
+        }
+        if (user.getName().isBlank() || user.getName() == null) {
+            user.setName(user.getLogin());
+        }
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -0,0 +1,44 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.controller.UserController;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserControllerTest {
+
+    @Test
+    void testUserValidationWithInvalidEmail() {
+        User userWithInvalidEmail = new User();
+        userWithInvalidEmail.setEmail("invalidemail");
+        userWithInvalidEmail.setLogin("validlogin");
+        userWithInvalidEmail.setBirthday(LocalDate.of(1990, 1, 1));
+
+        assertThrows(ValidationException.class, () -> UserController.validateUser(userWithInvalidEmail));
+    }
+
+    @Test
+    void testUserValidationWithInvalidLogin() {
+        User userWithInvalidLogin = new User();
+        userWithInvalidLogin.setEmail("validemail@example.com");
+        userWithInvalidLogin.setLogin("invalid login");
+        userWithInvalidLogin.setBirthday(LocalDate.of(1990, 1, 1));
+
+        assertThrows(ValidationException.class, () -> UserController.validateUser(userWithInvalidLogin));
+    }
+
+    @Test
+    void testUserValidationWithFutureBirthday() {
+        User userWithFutureBirthday = new User();
+        userWithFutureBirthday.setEmail("validemail@example.com");
+        userWithFutureBirthday.setLogin("validlogin");
+        userWithFutureBirthday.setBirthday(LocalDate.now().plusDays(1));
+
+        assertThrows(ValidationException.class, () -> UserController.validateUser(userWithFutureBirthday));
+    }
+}
