1dfad07d349f614afd06c2fa8514bbda85d86465
Magicoff <danilka.3012@yandex.ru>
2024-06-15 03:17:26 +0300

    done

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -56,16 +56,3 @@ create table if not exists mpa_films
     mpa_id  int
 );
 
-insert into genres(name) values ('Комедия');
-insert into genres(name) values ('Драма');
-insert into genres(name) values ('Мультфильм');
-insert into genres(name) values ('Триллер');
-insert into genres(name) values ('Документальный');
-insert into genres(name) values ('Боевик');
-
-insert into mpa(name) values ('G');
-insert into mpa(name) values ('NC-17');
-insert into mpa(name) values ('PG');
-insert into mpa(name) values ('PG-13');
-insert into mpa(name) values ('R');
-
